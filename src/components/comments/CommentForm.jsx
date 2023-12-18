c29708efbc5a582f8c5f88d614e2d265e680dd44
Jury Gregorio <jury.devs@gmail.com>
2023-12-18 18:36:55 +0800

    Fixed Bug in Comment

--- src/components/comments/CommentForm.jsx
+++ src/components/comments/CommentForm.jsx
@@ -1,32 +1,30 @@
-import React, { useState } from 'react';
-import './comment.css';
+import React, { useState } from "react";
+import "./comment.css";
 
 const CommentForm = ({ btnLabel, formSubmitHandler }) => {
-  const [value, setValue] = useState('');
-  //Submit handler function
-  const submitHandler = (e) => {
-    e.preventDefault();
-    formSubmitHandler(value);
-    setValue('');
-  };
-  return (
-    <form onSubmit={submitHandler}>
-      <div className="article-comment-form-wrapper">
-        <textarea
-          className="article-comment-form"
-          name=""
-          id=""
-          rows="5"
-          placeholder="Leave a comment"
-          value={value}
-          onChange={(e) => setValue(e.target.value)}
-        />
-        <button type="submit" className="article-comment-form-btn">
-          {btnLabel}
-        </button>
-      </div>
-    </form>
-  );
+   const [value, setValue] = useState("");
+   //Submit handler function
+   const submitHandler = (e) => {
+      e.preventDefault();
+      formSubmitHandler(value);
+      setValue("");
+   };
+   return (
+      <form onSubmit={submitHandler}>
+         <div className="article-comment-form-wrapper">
+            <textarea
+               className="article-comment-form"
+               rows="5"
+               placeholder="Leave a comment"
+               value={value}
+               onChange={(e) => setValue(e.target.value)}
+            />
+            <button type="submit" className="article-comment-form-btn">
+               {btnLabel}
+            </button>
+         </div>
+      </form>
+   );
 };
 
 export default CommentForm;
--- src/components/comments/CommentsContainer.jsx
+++ src/components/comments/CommentsContainer.jsx
@@ -1,72 +1,71 @@
-import React, { useEffect, useState } from 'react';
-import CommentForm from './CommentForm';
-import { getCommentsData } from '../../data/comments';
-import Comment from './Comment';
+import React, { useEffect, useState } from "react";
+import CommentForm from "./CommentForm";
+import { getCommentsData } from "../../data/comments";
+import Comment from "./Comment";
 
-const CommentsContainer = ({ className, logginedUserId }) => {
-  //creating a state with empty initial
-  const [comments, setComments] = useState([]);
+const CommentsContainer = ({ loggedinUserId }) => {
+   //creating a state with empty initial
+   const [comments, setComments] = useState([]);
 
-  //Getting the main comments/parents comments
-  //filtering the comments who have no parent
-  const mainComments = comments.filter((comment) => comment.parent === null);
-  const [afftectedComment, setAffectedComment] = useState(null);
-  console.log(comments);
+   //Getting the main comments/parents comments
+   //filtering the comments who have no parent
+   const mainComments = comments.filter((comment) => comment.parent === null);
+   const [affectedComment, setAffectedComment] = useState(null);
 
-  //   Creating a state to reply and edit a comment
+   //   Creating a state to reply and edit a comment
 
-  //fill the state with the data from getCommentsData(putt all data in it)
-  useEffect(() => {
-    (async () => {
-      const commentData = await getCommentsData();
-      setComments(commentData);
-    })();
-  }, []);
+   //fill the state with the data from getCommentsData(putt all data in it)
+   useEffect(() => {
+      (async () => {
+         const commentData = await getCommentsData();
+         setComments(commentData);
+      })();
+   }, []);
 
-  //this is for the comment handler
-  const addCommentHandler = (value, parent = null, replyOnUser = null) => {
-    //creating a variable newComment that stores the new comment
-    const newComment = {
-      _id: '10',
-      user: {
-        _id: 'a',
-        name: 'Mohammad Rezaii',
-      },
-      desc: value,
-      post: '1',
-      parent: parent,
-      replyOnUser: replyOnUser,
-      createdAt: '2022-12-31T17:22:05.092+0000',
-    };
+   //this is for the comment handler
+   const addCommentHandler = (value, parent = null, replyOnUser = null) => {
+      //creating a variable newComment that stores the new comment
+      const newComment = {
+         _id: "10",
+         user: {
+            _id: "a",
+            name: "Mohammad Rezaii",
+         },
+         desc: value,
+         post: "1",
+         parent: parent,
+         replyOnUser: replyOnUser,
+         createdAt: "2022-12-31T17:22:05.092+0000",
+      };
 
-    //adding comments to comments data
-    setComments((curState) => {
-      return [newComment, ...curState];
-    });
-  };
+      //adding comments to comments data
+      setComments((curState) => {
+         return [newComment, ...curState];
+      });
+   };
 
-  return (
-    <div className={`${className}`}>
-      <CommentForm
-        btnLabel="Add Comment"
-        //passing the formSubmitHandler function with addCommentHandler
-        formSubmitHandler={(value) => addCommentHandler(value)}
-      />
+   return (
+      <div className="comments-container">
+         <CommentForm
+            btnLabel="Add Comment"
+            //passing the formSubmitHandler function with addCommentHandler
+            formSubmitHandler={(value) => addCommentHandler(value)}
+         />
 
-      <div className="comments-area">
-        {/* rendering the main comments/parents comments fromt the mainComments */}
-        {mainComments.map((comment) => (
-          <Comment
-            comment={comment}
-            logginedUserId={logginedUserId}
-            afftectedComment={afftectedComment}
-            setAffectedComment={setAffectedComment}
-            addComment={addCommentHandler}
-          />
-        ))}
+         <div className="comments-area">
+            {/* rendering the main comments/parents comments fromt the mainComments */}
+            {mainComments.map((comment) => (
+               <Comment
+                  comment={comment}
+                  loggedinUserId={loggedinUserId}
+                  affectedComment={affectedComment}
+                  setAffectedComment={setAffectedComment}
+                  addComment={addCommentHandler}
+               />
+            ))}
+         </div>
       </div>
-    </div>
-  );
+   );
 };
 
 export default CommentsContainer;
